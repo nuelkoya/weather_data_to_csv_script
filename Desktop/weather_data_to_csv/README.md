17a5029728597f370c8a439a4e42baf6700419ad
Adetola <Adetola442@gmail.com>
2025-09-05 21:09:03 -0500

    added readme

---
+++ Desktop/weather_data_to_csv/README.md
@@ -0,0 +1,29 @@
+My Weather Details
+------------------------------------------------------------------------------------
+A simple Python CLI app that fetches real-time weather data for any city using the OpenWeatherMap API and stores the results in a CSV file for future reference.
+
+
+Features
+------------------------------------------------------------------------------------
+Fetch current weather by city name
+
+Get temperature, weather description, humidity, and wind speed
+Includes date and time for every record
+Saves results to a CSV file (my-weather-details.csv)
+Automatically appends new records without overwriting old ones
+
+
+Usage
+------------------------------------------------------------------------------------
+Get your OpenWeatherMap API key
+Sign up on OpenWeatherMap
+Go to your dashboard and generate an API key
+Replace the API_KEY variable in main.py with your key.
+
+
+Installation
+------------------------------------------------------------------------------------
+git clone https://github.com/nuelkoya/weather_data_to_csv_script.git
+cd weather_data_to_csv
+pip install requests
+
---
+++ Desktop/weather_data_to_csv/main.py
@@ -0,0 +1,64 @@
+import requests
+from datetime import datetime
+import csv
+import os
+
+API_KEY = "bf09c3306fec22e396ee9db9afb9181e"
+HEADERS = ["City", "Temperature (C)", "Weather Description","Humidity", "Wind Speed", "Date", "Time"]
+searched_cities_data = []
+FILE = "my-weather-details.csv"
+
+# Function to get weather data for user inputted city from API
+def get_weather_data(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    try:
+        res = requests.get(url)
+        city_data = []
+
+        if (res.status_code != 200):
+            print(f"Request Error: {res.status_code}")
+        else:
+            city_weather_data = res.json()
+            date_time_object = datetime.fromtimestamp(city_weather_data['dt'])
+            date, time = date_time_object.strftime('%d %b %Y,%I:%M%p').split(',')
+            city_data.append([
+                city,
+                city_weather_data['main']['temp'],
+                city_weather_data['weather'][0]['description'],
+                city_weather_data['main']['humidity'],
+                city_weather_data['wind']['speed'],
+                date,
+                time
+            ])
+            return city_data
+    except requests.exceptions.RequestException as error:
+        print(f'Error: {error}')
+
+
+# Function to write weather data to csv file
+def write_to_csv(data):
+    if (os.path.exists(FILE)):
+        with open(FILE, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
+    else:
+        with open(FILE, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(HEADERS)
+            writer.writerows(data)
+
+    
+
+def main():
+    while True:
+        city = input('Enter a city: ')
+        input_city_data = get_weather_data(city)
+        if input_city_data:
+            write_to_csv(input_city_data)
+            break
+        else:
+            print('Something went wrong! Make sure you entered the right city name')
+
+
+if __name__ == '__main__':
+    main()
